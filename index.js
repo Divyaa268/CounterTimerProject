dc3e734d9235acadcc5b6fa2c220e2f34ea63b82
Divyaa268 <117614772+Divyaa268@users.noreply.github.com>
2023-05-21 16:12:04 +0530

    Updated index.js
    Updated index.js with timerFunction()

---
+++ index.js
@@ -0,0 +1,16 @@
+// calculating everything in millisecond
+const second = 1000;
+const minute = 60 * second;
+const hour = 60 * minute;
+const day = 24 * hour;
+
+
+const timerFunction = () => {
+    
+    console.log(second);
+    console.log(minute);
+    console.log(hour);
+    console.log(day);
+};
+
+timerFunction();
